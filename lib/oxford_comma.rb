6a5fce0218c468af716ff15ac8cf324b7daa3ecc
Ricky Liu <rorix12321@gmail.com>
2018-08-12 12:53:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  string1 = ""
+    if array.length == 1
+        string1 << "#{array[0]}"
+    elsif array.length == 2
+      string1 << "#{array[0]} and #{array[1]}"
+    else
+      array.each_with_index do |x, index|
+        if index == 0
+          string1 << "#{x}, "
+        elsif index == array.length - 1
+          string1 << "and #{x}"
+        else
+          string1 << "#{x}, "
+        end
+      end
+    end
+    return string1
 end
